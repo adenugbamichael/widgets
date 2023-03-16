896264b0c08dd3f5fcbc001da7113556b08972ff
adenugbamichael <adenugbamicke@gmail.com>
2023-03-16 22:39:18 +0100

    making use of useRef | body event listener

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false)
+  const ref = useRef()
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return
+      }
+      setOpen(false)
+    }
+
+    document.body.addEventListener("click", onBodyClick)
+    return () => {
+      document.body.removeEventListener("click", onBodyClick)
+    }
+  }, [])
 
   const renderedOptions = options.map((option) => {
     if (option.image === selected.image) {
@@ -17,11 +32,12 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       </div>
     )
   })
+
   return (
-    <div className='ui form'>
+    <div ref={ref} className='ui form'>
       <div className='field'>
         <label htmlFor='' className='label'>
-          Select a Color
+          Select a Country
         </label>
         <div
           onClick={() => setOpen(!open)}
