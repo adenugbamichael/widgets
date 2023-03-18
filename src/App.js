836a4c4a5732a8b249380aa304f3746d35d2c486
adenugbamichael <adenugbamicke@gmail.com>
2023-03-18 06:01:03 +0100

    extract routing logic into reusable component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,59 @@ import Search from "./components/Search"
 import Dropdown from "./components/Dropdown"
 import Translate from "./components/Translate"
 import { items, options } from "./store"
+import Route from "./components/Route"
+
+const showAccordion = (accordion) => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />
+  }
+}
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />
+  }
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />
+  }
+}
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />
+  }
+}
+
+// const showComponents = (route, component) => {
+//   return window.location.pathname === route ? component : null
+// }
 
 function App() {
-  // const [selected, setSelected] = useState(options[0])
+  const [selected, setSelected] = useState(options[0])
   // const [showDropdown, setShowDropdown] = useState(true)
 
   return (
     <div>
-      {/* <Accordion items={items} />{" "} */}
-      {/* <Search /> */}
-      {/* <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
         <Dropdown
+          country='Select a country'
+          options={options}
           selected={selected}
           onSelectedChange={setSelected}
-          options={options}
         />
-      ) : null} */}
-
-      <Translate />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     </div>
   )
 }
